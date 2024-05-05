756212354fc34e1ab2eaffdd46252b32ee2e586d
Muhammad Wael <muhammad.wael2040@gmail.com>
2024-05-05 22:08:17 +0300

    modify file_access and adding std_lib.h

--- data/file_access/file_access.c
+++ data/file_access/file_access.c
@@ -9,7 +9,22 @@
 /* ******************** Include Section End ******************** */
 
 /* ***************** Global Sub-program Start ****************** */
-
+bool write_data_to_admins (Admin *admins, uint16 size)
+{
+    /* write_data_to_admins Function used to write to admins.csv
+     * it takes array of admins and size of array */
+    FILE *file = fopen("admins.csv", "w"); /* open admins.csv in write mode */
+    if (file == NULL)
+        return false;
+    int iter; /* to iterate through Array of Admins */
+    fprintf(file, "ID, Name, Password\n");
+    for (iter = 0; iter < size; ++iter) {
+        fprintf(file, "%d, %s, %s", admins[iter].ID, admins[iter].name, admins[iter].password);
+        if (iter != size - 1)
+            fprintf(file, "\n");
+    }
+    return true;
+}
 /* ****************** Global Sub-program End ******************* */
 
 /* ***************** History Log Section Start ***************** */
--- data/std_lib.h
+++ data/std_lib.h
@@ -1,22 +1,53 @@
 /* **************** Documentation Section Start **************** */
-/*        File Name   :  file_access.h                           */
+/*        File Name   :  std_lib.h                               */
 /*        Author      :  Muhammad Wael                           */
-/*        Description :  Functions to read and write data        */
+/*        Description :  Standard Functions and                  */
 /* ***************** Documentation Section End ***************** */
 
-#ifndef FILE_ACCESS_H_
-#define FILE_ACCESS_H_
+#ifndef STD_LIB_
+#define STD_LIB_
 
 /* ******************* Include Section Start ******************* */
-#include "../std_lib.h"
-#include <stdio.h>
+
 /* ******************** Include Section End ******************** */
 
 /* ******************** Macro Section Start ******************** */
-
+#define bool _Bool
+#define false 0
+#define true 1
 /* ********************* Macro Section End ********************* */
 
 /* ******************* Typedef Section Start ******************* */
+
+typedef unsigned char         uint8;
+typedef unsigned short        uint16;
+typedef unsigned long         uint32;
+typedef unsigned long long    uint64;
+typedef signed char           int8;
+typedef signed short          int16;
+typedef signed long           int32;
+typedef signed long long      int64;
+typedef float                 float32;
+typedef double                float64;
+
+typedef struct {
+    uint16 ID;
+    uint8 name[20];
+    uint8 gender[8];
+    uint8 age;
+    uint8 total_grade;
+} Record;
+
+typedef struct {
+    uint16 ID;
+    uint8 name[20];
+    uint8 password[20];
+} Admin;
+
+typedef struct {
+    uint16 ID;
+    uint8 password[20];
+} Student;
 
 /* ******************** Typedef Section End ******************** */
 
@@ -26,16 +57,13 @@
 
 /* ************** Global Declaration Section Start ************* */
 
-bool write_data_to_admins (Admin *admins, uint16 size);
-
-Admin read_data_from_admins ();
-
 /* *************** Global Declaration Section End ************** */
 
 #endif
 
 /* ***************** History Log Section Start ***************** */
 /*  User                   Date                     Brief
- *  Muhammad Wael          3/5/2024 23:18           Adding File Layout
+ *  Muhammad Wael          5/5/2024 21:41           Adding File Layout
+ *  Muhammad Wael          5/5/2024 22:00           Adding typedef section
  */
 /* ****************** History Log Section End ****************** */
