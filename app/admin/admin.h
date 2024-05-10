acf06972ea9aabac8e32b0df566aec7b54615cbb
Muhammad Wael <muhammad.wael2040@gmail.com>
2024-05-11 00:20:15 +0300

    add edit pass and grade functions

--- app/admin/admin.h
+++ app/admin/admin.h
@@ -11,6 +11,8 @@
 
 #include "../../data/std_lib.h"
 #include "../../data/file_access/file_access.h"
+#include "../../data/memory_management/memory_management.h"
+#include "../../data/data_management/data_management.h"
 
 /* ******************** Include Section End ******************** */
 
@@ -29,6 +31,8 @@
 /* ************** Global Declaration Section Start ************* */
 
 void view_student_record (uint32 ID);
+bool edit_grade (uint32 ID);
+bool edit_password_of_admin (uint32 ID);
 
 /* *************** Global Declaration Section End ************** */
 
@@ -38,5 +42,6 @@ void view_student_record (uint32 ID);
 /*  User                   Date                     Brief
  *  Muhammad Wael          10/5/2024 00:45           Adding File Layout
  *  Muhammad Wael          10/5/2024 22:41           Adding view student record function
+ *  Muhammad Wael          11/5/2024 00:11           Adding edit password and edit grade functions
  */
 /* ****************** History Log Section End ****************** */
