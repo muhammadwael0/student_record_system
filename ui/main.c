77efdaef8c1caddc9b89428c8f36369a5cdcb7fc
GMina356 <168474477+GMina356@users.noreply.github.com>
2024-05-11 23:37:01 +0300

    Adding File Layout

---
+++ ui/main.c
@@ -0,0 +1,21 @@
+/* **************** Documentation Section Start **************** */
+/*        File Name   :  main.c                                  */
+/*        Author      :  Mina Nabil                              */
+/*        Description :  Functions for main                      */
+/* ***************** Documentation Section End ***************** */
+
+/* ******************* Include Section Start ******************* */
+#include "main.h"
+/* ******************** Include Section End ******************** */
+
+/* ***************** Global Sub-program Start ****************** */
+
+
+
+/* ****************** Global Sub-program End ******************* */
+
+/* ***************** History Log Section Start ***************** */
+/*  User                   Date                     Brief
+ *  Mina Nabil             11/5/2024 23:28          Adding File Layout
+ * */
+/* ****************** History Log Section End ****************** */
---
+++ ui/main.h
@@ -0,0 +1,46 @@
+/* **************** Documentation Section Start **************** */
+/*        File Name   :  main.h                                  */
+/*        Author      :  Mina Nabil                              */
+/*        Description :  Functions for main                      */
+/* ***************** Documentation Section End ***************** */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/* ******************* Include Section Start ******************* */
+
+#include "../data/std_lib.h"
+#include "../data/file_access/file_access.h"
+#include "../data/memory_management/memory_management.h"
+#include "../data/data_management/data_management.h"
+#include "../app/admin/admin.h"
+#include "../app/authentication/authentication.h"
+#include "../app/student/student.h"
+
+/* ******************** Include Section End ******************** */
+
+/* ******************** Macro Section Start ******************** */
+
+/* ********************* Macro Section End ********************* */
+
+/* ******************* Typedef Section Start ******************* */
+
+/* ******************** Typedef Section End ******************** */
+
+/* *************** Global Variables Section Start ************** */
+
+/* **************** Global Variables Section End *************** */
+
+/* ************** Global Declaration Section Start ************* */
+
+
+
+/* *************** Global Declaration Section End ************** */
+
+#endif
+
+/* ***************** History Log Section Start ***************** */
+/*  User                   Date                     Brief
+ *  Mina Nabil             11/5/2024 23:35          Adding File Layout
+ */
+/* ****************** History Log Section End ****************** */
