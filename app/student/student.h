b68f34d2a572243c8b2f9db1bedfdab6ff74159b
GMina356 <168474477+GMina356@users.noreply.github.com>
2024-05-11 20:13:15 +0300

    Adding File Layout

---
+++ app/student/student.h
@@ -0,0 +1,43 @@
+/* **************** Documentation Section Start **************** */
+/*        File Name   :  student.h                               */
+/*        Author      :  Mina Nabil                              */
+/*        Description :  Functions for student                   */
+/* ***************** Documentation Section End ***************** */
+
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
+/* ******************* Include Section Start ******************* */
+
+#include "../../data/std_lib.h"
+#include "../../data/file_access/file_access.h"
+#include "../../data/memory_management/memory_management.h"
+#include "../../data/data_management/data_management.h"
+
+/* ******************** Include Section End ******************** */
+
+/* ******************** Macro Section Start ******************** */
+
+/* ********************* Macro Section End ********************* */
+
+/* ******************* Typedef Section Start ******************* */
+
+/* ******************** Typedef Section End ******************** */
+
+/* *************** Global Variables Section Start ************** */
+
+/* **************** Global Variables Section End *************** */
+
+/* ************** Global Declaration Section Start ************* */
+
+
+
+/* *************** Global Declaration Section End ************** */
+
+#endif
+
+/* ***************** History Log Section Start ***************** */
+/*  User                   Date                     Brief
+ *  Mina Nabil             11/5/2024 20:10          Adding File Layout
+ */
+/* ****************** History Log Section End ****************** */
