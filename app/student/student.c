0d6818245e72f731574a7291b55385f57b22f758
GMina356 <168474477+GMina356@users.noreply.github.com>
2024-05-11 20:17:02 +0300

    Adding File Layout

---
+++ app/student/student.c
@@ -0,0 +1,21 @@
+/* **************** Documentation Section Start **************** */
+/*        File Name   :  student.c                               */
+/*        Author      :  Mina Nabil                              */
+/*        Description :  Functions for student                   */
+/* ***************** Documentation Section End ***************** */
+
+/* ******************* Include Section Start ******************* */
+#include "student.h"
+/* ******************** Include Section End ******************** */
+
+/* ***************** Global Sub-program Start ****************** */
+
+
+
+/* ****************** Global Sub-program End ******************* */
+
+/* ***************** History Log Section Start ***************** */
+/*  User                   Date                     Brief
+ *  Mina Nabil             11/5/2024 20:15          Adding File Layout
+ * */
+/* ****************** History Log Section End ****************** */
